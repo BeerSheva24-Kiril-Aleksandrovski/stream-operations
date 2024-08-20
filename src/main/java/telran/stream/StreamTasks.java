3af6edcc0d4e85db24036518c546e5701251c3e7
Kiril <fanky.yahoo@gmail.com>
2024-08-20 12:11:33 +0300

    One pipeline applyed

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,6 +1,5 @@
 package telran.stream;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class StreamTasks {
@@ -10,11 +9,11 @@ public class StreamTasks {
         //with all numbers from a given array but with different order
         //aech method call returns new array in some random order
         //IntStream stream = Arrays.stream(arr);
-        int [] randomIndex = new Random().ints(0, arr.length)
+        int [] shuffledArray = new Random().ints(0, arr.length)
                                          .distinct()
                                          .limit(arr.length)
+                                         .map(i -> arr[i])
                                          .toArray();
-        int [] shuffledArray = Arrays.stream(randomIndex).map(i -> arr[i]).toArray();                              
         return shuffledArray;
     }
 }
